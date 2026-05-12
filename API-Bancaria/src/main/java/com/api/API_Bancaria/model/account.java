a457f85d3ff24c3165687b1327243b3a7c6310dc
igorcarvalho <cigor0089@gmail.com>
2026-05-12 17:33:00 -0300

    feat: creating account entity

---
+++ API-Bancaria/src/main/java/com/api/API_Bancaria/model/account.java
@@ -0,0 +1,30 @@
+package com.api.API_Bancaria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long account_Id;
+
+    private String account_name;
+    private String account_cpf;
+    private BigDecimal account_balance;
+    private String account_agency;
+    private String number;
+}
