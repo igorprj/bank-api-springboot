55b8624458f624d516d91d1e98249f21ed597a29
igorcarvalho <cigor0089@gmail.com>
2026-05-12 17:21:05 -0300

    chore: Initial project setup

---
+++ API-Bancaria/src/main/java/com/api/API_Bancaria/ApiBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.api.API_Bancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancariaApplication.class, args);
+	}
+
+}
---
+++ API-Bancaria/src/test/java/com/api/API_Bancaria/ApiBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.API_Bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
