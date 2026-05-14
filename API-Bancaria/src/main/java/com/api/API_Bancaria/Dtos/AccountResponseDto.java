2612d183eb9d2aecf9ff53c0109db0ccdd9a3a63
igorcarvalho <cigor0089@gmail.com>
2026-05-14 09:10:07 -0300

    feat: create services and DTOs

---
+++ API-Bancaria/src/main/java/com/api/API_Bancaria/Dtos/AccountResponseDto.java
@@ -0,0 +1,19 @@
+package com.api.API_Bancaria.Dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class AccountResponseDto {
+
+    private Long account_Id;
+
+    private String account_name;
+    private String account_cpf;
+    private BigDecimal account_balance;
+    private String account_agency;
+    private String number;
+}
--- API-Bancaria/src/main/java/com/api/API_Bancaria/Repositories/AccountRepository.java
+++ API-Bancaria/src/main/java/com/api/API_Bancaria/Repositories/AccountRepository.java
@@ -2,6 +2,8 @@ package com.api.API_Bancaria.Repositories;
 
 import com.api.API_Bancaria.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
